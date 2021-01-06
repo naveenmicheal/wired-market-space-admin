2534c841aa17a5afb071f7c0d79672ed88739356
zero <naveenmichael.alpha.local>
2021-01-07 01:31:42 +0530

    update

--- dist/_nuxt/static/1608908103/transactions/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/transactions", {data:[{}],fetch:[],mutations:[]});
---
+++ dist/_nuxt/static/1609963119/addproduct/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1609963119",layout:"default",error:null,state:{products:{products:[],categories:[],totalorders:a,totalprice:a}},serverRendered:true,routePath:"\u002Faddproduct",config:{}}}(""));
